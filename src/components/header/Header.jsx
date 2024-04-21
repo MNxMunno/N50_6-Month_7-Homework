fbb821b5ed9e7b7fd78e3f915fd7e69269d5363c
MNxMunno <bilolhuzayfa@gmmail.com>
2024-04-21 19:16:57 +0500

    change

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,8 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, NavLink } from "react-router-dom";
 import Logo from "../../assets/images/Logo.svg";
-import NavLinks from "../../static/navLink/NavLinks";
+// import NavLinks from "../../static/navLink/NavLinks";
 import { GiHamburgerMenu } from "react-icons/gi";
 
 const Header = () => {
@@ -15,7 +15,24 @@ const Header = () => {
           <a className="logo" href="#" onClick={() => navigation("/")}>
             <img src={Logo} alt="log" />
           </a>
-          <NavLinks className={`nav__items ${toggel ? "show" : ""}`} />
+          <div className={`nav__items ${toggel ? "show" : ""}`}>
+            <NavLink className="nav__link" to={"/"}>
+              Home
+            </NavLink>
+            <NavLink className="nav__link" to={"/about"}>
+              About
+            </NavLink>
+            <NavLink className="nav__link" to={"/services"}>
+              Services
+            </NavLink>
+            <NavLink className="nav__link" to={"/careers"}>
+              Careers
+            </NavLink>
+            <NavLink className="nav__link" to={"/blog"}>
+              Blog
+            </NavLink>
+            <button className="nav__btn">Clone project</button>
+          </div>
           <button onClick={() => setToggel(!toggel)}>
             <GiHamburgerMenu className="menu" />
           </button>
--- src/static/navLink/NavLinks.jsx
+++ src/static/navLink/NavLinks.jsx
@@ -1,28 +1,9 @@
-// eslint-disable-next-line no-unused-vars
-import React from "react";
-import { NavLink } from "react-router-dom";
+// // eslint-disable-next-line no-unused-vars
+// import React from "react";
+// import { NavLink } from "react-router-dom";
 
-const NavLinks = () => {
-  return (
-    <div className="nav__links">
-      <NavLink className="nav__link" to={"/"}>
-        Home
-      </NavLink>
-      <NavLink className="nav__link" to={"/about"}>
-        About
-      </NavLink>
-      <NavLink className="nav__link" to={"/services"}>
-        Services
-      </NavLink>
-      <NavLink className="nav__link" to={"/careers"}>
-        Careers
-      </NavLink>
-      <NavLink className="nav__link" to={"/blog"}>
-        Blog
-      </NavLink>
-      <button className="nav__btn">Clone project</button>
-    </div>
-  );
-};
+// const NavLinks = () => {
+//   return <div className="nav_links"></div>;
+// };
 
-export default NavLinks;
+// export default NavLinks;
